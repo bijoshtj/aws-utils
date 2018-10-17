a0815f473563e49832ffccb337997d69715031c7
Bijosh T J <bijosh.tj.contractor@move.com>
2018-10-17 17:55:40 +0530

    support multiple kinesis shrad fetch from the kinesis stream

--- kinesis_fetch.js
+++ kinesis_fetch.js
@@ -2,15 +2,16 @@
 let AWS = require('aws-sdk');
 let config = require('./config/kinesis');
 
+let shrads = config.shrads;
 let kinesis = new AWS.Kinesis({
   region: config.region
 });
 
-let getShradIteratorParams = function () {
+let getShradIteratorParams = function (shrad_id) {
   let iterator_type = config.shrad_iterator_type;
   let params = {
     StreamName: config.stream_name,
-    ShardId: config.shrad_id,
+    ShardId: shrad_id,
     ShardIteratorType: iterator_type
   };
 
@@ -23,30 +24,36 @@ let getShradIteratorParams = function () {
   return params;
 };
 
-kinesis.getShardIterator(getShradIteratorParams(), (err, data) => {
-  if (!err) {
-    console.log('shrad iterator resp: ', data);
-    kinesis.getRecords(data, (err, data) => {
-      if (!err) {
-        console.log("========= get records =========");
-        if (data && data.Records && data.Records.length > 0) {
-            console.log("Total records in egress kinesis: ", data.Records.length);
-          for (let i = 0; i < data.Records.length; i++) {
-            let curr = data.Records[i];
-
-            console.log("\n\n+++++ Record: ", i+1, " ++++++");
-            console.log("PartitionKey: ", curr.PartitionKey);
-            console.log("ArrivalTime: ", curr.ApproximateArrivalTimestamp);
-            console.log("Data: ", curr.Data.toString('utf8'));
+let fetchRecordsForShrad = function (shradId) {
+  kinesis.getShardIterator(getShradIteratorParams(shradId), (err, data) => {
+    if (!err) {
+      console.log('shrad iterator resp: ', data);
+      kinesis.getRecords(data, (err, data) => {
+        if (!err) {
+          console.log("========= get records =========");
+          if (data && data.Records && data.Records.length > 0) {
+              console.log("Total records in egress kinesis: ", data.Records.length);
+            for (let i = 0; i < data.Records.length; i++) {
+              let curr = data.Records[i];
+  
+              console.log("\n\n+++++ Record: ", i+1, " ++++++");
+              console.log("PartitionKey: ", curr.PartitionKey);
+              console.log("ArrivalTime: ", curr.ApproximateArrivalTimestamp);
+              console.log("Data: ", curr.Data.toString('utf8'));
+            }
+          } else {
+            console.log("No records found!!!!");
           }
         } else {
-          console.log("No records found!!!!");
+          console.log('error get records: ', err);
         }
-      } else {
-        console.log('error get records: ', err);
-      }
-    });
-  } else {
-    console.log('get shrad iteratoor error ', err);
-  }
-})
+      });
+    } else {
+      console.log('get shrad iteratoor error ', err);
+    }
+  });
+};
+
+if (shrads && shrads.length > 0) {
+  shrads.forEach(fetchRecordsForShrad);
+}
